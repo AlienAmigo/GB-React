e1842a1ca6cf61544e38c6cc5aa4872fd7c14abb
Pyotr Ryzhov <maestro_pete@inbox.ru>
2021-12-11 19:21:58 +0300

    added 5th task

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { AppBox, ContentBox } from 'components/AppWrapper/style';
 import Profile from 'views/Profile';
@@ -7,22 +8,25 @@ import Home from 'views/Home';
 import Navigation from 'components/Navigation';
 import { ROUTERS } from 'router';
 import NotMatch from 'views/NotMatch';
+import { store } from 'store';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <AppBox>
-        <Navigation />
-        <ContentBox>
-          <Switch>
-            <Route component={Profile} path={ROUTERS.PROFILE} />
-            <Route component={Chats} path={ROUTERS.CHATS} />
-            <Route component={Home} exact path={ROUTERS.BASE} />
-            <Route component={NotMatch} path="*" />
-          </Switch>
-        </ContentBox>
-      </AppBox>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppBox>
+          <Navigation />
+          <ContentBox>
+            <Switch>
+              <Route component={Profile} path={ROUTERS.PROFILE} />
+              <Route component={Chats} path={ROUTERS.CHATS} />
+              <Route component={Home} exact path={ROUTERS.BASE} />
+              <Route component={NotMatch} path="*" />
+            </Switch>
+          </ContentBox>
+        </AppBox>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
