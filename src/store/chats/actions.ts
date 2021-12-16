694535d6628346e264d623d2fc0130e1d24f2d71
Pyotr Ryzhov <maestro_pete@inbox.ru>
2021-12-16 19:54:17 +0300

    fixed

--- src/store/chats/actions.ts
+++ src/store/chats/actions.ts
@@ -1,15 +1,13 @@
-import { Chat } from '@mui/icons-material';
-
 export const CREATE_CHAT = 'CREATE_CHAT';
 export const SET_CHATS = 'SET_CHATS';
 export const REMOVE_CHAT = 'REMOVE_CHAT';
 
-export const createChat = (chat: object) => ({
+export const createChat = (chat: any) => ({
   type: CREATE_CHAT,
   payload: chat
 });
 
-export const setChats = (chats: object) => ({
+export const setChats = (chats: any) => ({
   type: SET_CHATS,
   payload: chats
 });
